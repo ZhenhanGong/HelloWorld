6e11cc62451ff2ccde01d536de464acd8b931c74
zhenhan gong <zhenhan.gong@gmail.com>
2020-05-20 20:45:13 +0800

    comment out mac <optional>

--- main.cpp
+++ main.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
-#include <string>
-#include <optional>
+/* #include <string> */
+/* #include <optional> */
 
-std::optional<std::string> test_optional(bool b)
-{
-  std::string str = "string";
-  if (b)
-    return str;
-  else
-    return {};
-}
+/* std::optional<std::string> test_optional(bool b) */
+/* { */
+/*   std::string str = "string"; */
+/*   if (b) */
+/*     return str; */
+/*   else */
+/*     return {}; */
+/* } */
 
 int main()
 {
     std::cout << "Hello World!" << std::endl;
-    auto res = test_optional(true);
-    std::cout << *res << std::endl;
+    /* auto res = test_optional(true); */
+    /* std::cout << *res << std::endl; */
     return 0;
 }
